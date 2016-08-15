17c6482a09d33fae74c2b36f9bf817b068729c09
Rayson Zhu <yuxzhu@redhat.com>
2016-08-15 22:24:15 +0800

    add C module 'procszoo.c_functions.atfork'
    This module is a wrapper to C function pthread_atfork(3).
    Writing this module because ctypes is buggy to deal with this function.

---
+++ procszoo/c_functions/atfork/atfork.h
@@ -0,0 +1,6 @@
+#ifndef _PROCSZOO_CFUNCTIONS_ATFORK_H_
+#define _PROCSZOO_CFUNCTIONS_ATFORK_H_
+
+PyObject *atfork_pthread_atfork(PyObject *self, PyObject *args);
+
+#endif /* _PROCSZOO_CFUNCTIONS_ATFORK_H_ */
